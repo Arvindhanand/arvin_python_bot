f44014499c148be729d50123d967f159f2cbd605
Arvindhanand <96776038+Arvindhanand@users.noreply.github.com>
2022-10-15 19:38:49 +0530

    Add files via upload
    Bot first commit

---
+++ petrol_weather_BOT.py
@@ -0,0 +1,106 @@
+import pandas as pd
+import numpy as np
+from datetime import date
+from datetime import timedelta
+
+  
+# Returns the current local date
+today = date.today()
+yesterday=today-timedelta(days = 1)
+print("Today date is: ", today,yesterday)
+############### Petrol web scraping ################
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+
+def getdata(url):
+    r=requests.get(url)
+    return r.text
+htmldata = getdata("https://www.goodreturns.in/petrol-price.html")
+soup = BeautifulSoup(htmldata,'html.parser')
+mydata = ''
+result = []
+
+for table in soup.find_all('tr'):
+    mydata += table.get_text()
+
+mydata = mydata[1:]
+iteml = mydata.split("\n\n")
+for item in iteml[:-5]:
+    result.append(item.split("\n"))
+df = pd.DataFrame(result[:-8])
+df['City']=(df[0])
+df['Today']=(df[1])
+df['Yesterday']=(df[2])
+
+y=df[['City','Yesterday','Today']]
+Result=y[1:]
+Result
+x=Result.reset_index(drop=True)
+print(x)
+
+############### Weather web scraping ################
+from bs4 import BeautifulSoup
+import requests
+import numpy as np
+page = requests.get("https://forecast.weather.gov/MapClick.php?lat=37.7772&lon=-122.4168")
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+forecast = soup.find(id="seven-day-forecast")
+forecast_items = forecast.find_all(class_="tombstone-container")
+tonight = forecast_items[1]
+
+#forecast
+period = tonight.find(class_="period-name").get_text()
+description = tonight.find(class_="short-desc").get_text()
+temp = tonight.find(class_="temp").get_text()
+#print(period)
+#print(description)
+#print(temp)
+
+img = tonight.find("img")
+desc = img['title']
+#print(desc)
+period_tags = forecast.select(".tombstone-container .period-name")
+periods = [pt.get_text() for pt in period_tags]
+#periods
+short_desc = [sd.get_text() for sd in forecast.select(".tombstone-container .short-desc")]
+temps = [t.get_text() for t in forecast.select(".tombstone-container .temp")]
+temp = [t.get_text() for t in forecast.select(".tombstone-container .temp")]
+desc = [d["title"] for d in forecast.select(".tombstone-container img")]
+#print(short_desc)
+#print(temps)
+#print(desc)
+import pandas as pd
+weather = pd.DataFrame({"Period": periods,"Title": short_desc,"Temperature/F": temps,"Details":desc})
+#weather
+weather['Temperature'] = weather['Temperature/F'].str.extract('(\d+)').astype(int)
+
+weather['Variance']= weather['Temperature']-(np.mean(weather['Temperature']))
+print(weather)
+weather
+
+
+import telebot
+token = '1885204428:AAEsNFt184Nf_lLeh5HbQGltl5JDeTD8HJ4'
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=["help"])
+def send_help(message):
+    bot.reply_to(message,"Hello welcome, please click on /petrol or /weather to get petrol and weather info.\n Click on /exit to get the exit the session.\n Thank You!")
+    
+@bot.message_handler(commands=["petrol"])
+def echo_all(message):
+    bot.reply_to(message,f" Petrol Price as on {today} and {yesterday} \n{x},{message.text} \n Click on /weather to get the weather info.\n Click on /exit to exit the session")
+    
+@bot.message_handler(commands=["weather"])
+def echo_all(message):
+    bot.reply_to(message,f" Weather Report California \n{weather},{message.text}\n Click on /petrol to get the petrol price info.\n Click /exit to exit the session")    
+
+@bot.message_handler(commands=["exit"])
+def echo_all(message):
+    bot.reply_to(message,f"Thank you for using the BOT!\n Have a nice day!")    
+    
+    
+bot.polling()   
